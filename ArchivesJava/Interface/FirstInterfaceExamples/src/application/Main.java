43f60fbda0f6edde0173debe955728bb0bcac86d
ivaan <iat1@discente.ifpe.edu.br>
2024-06-09 22:05:29 -0300

    No interface solution

---
+++ ArchivesJava/Interface/FirstInterfaceExamples/src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Enter rental data: ");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Car rental pickup (dd/MMM/yyyy): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Devolution (dd/MMM/yyyy): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		System.out.print("Enter hour price: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Enter daily price: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("INVOICE: ");
+		System.out.println("Basic payment: " + cr.getInvoice().getBasicPayment());
+		System.out.println("Tax: " + cr.getInvoice().getTax());
+		System.out.println("Total payment: " + cr.getInvoice().getTotalPayment());
+	}
+
+}
---
+++ ArchivesJava/Interface/FirstInterfaceExamples/src/model/services/RentalService.java
@@ -0,0 +1,23 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		carRental.setInvoice(new Invoice(50.0, 10.0));
+	}
+}
